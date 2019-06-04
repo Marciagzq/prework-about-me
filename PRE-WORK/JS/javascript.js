a14123d54d149b9986049eb9f91491206149b53a
Marciagzq <50253500+Marciagzq@users.noreply.github.com>
2019-06-03 22:09:51 -0400

    Add files via upload

---
+++ PRE-WORK/JS/javascript.js
@@ -0,0 +1,28 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+document.getElementById("box").style.height = "500px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+document.getElementById("box").style.backgroundColor = "rgb(191, 233, 190)";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+document.getElementById("box").style.opacity = "0.2";
+
+});
+
+
+document.getElementById("button4").addEventListener("click", function(){
+
+document.getElementById("box").style.height = "150px";
+document.getElementById("box").style.backgroundColor = "orange";
+document.getElementById("box").style.opacity = "100";
+
+});
+
+
